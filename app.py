f1f24af12a3ab6e96495dbf7bca37bba55257e2f
Marjoelynne Torgo <marjtorgo@gmail.com>
2020-06-28 12:47:57 +0800

    First Commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+# Can use Postgre or MySQL
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
+# Posts and Gets
+def index():
+
+    if request.method == 'POST':
+        # pass
+        # return "Hello"
+        task_content = request.form['content']
+    else:
+        return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
